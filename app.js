e4d3763032e11de581f9ae710184cc13ff7549e5
Andrew <andrewwee97@gmail.com>
2021-11-17 01:23:37 +0800

    revert to base

--- app.js
+++ app.js
@@ -57,29 +57,29 @@ function sendEventsToAll(newFact) {
   clients.forEach(client => client.response.write(`data: ${JSON.stringify(newFact)}\n\n`))
 }
 
-var d2gsi = require('dota2-gsi');
-var server = new d2gsi(
-  {
-    port: process.env.PORT || 3000
-  }
-);
+// var d2gsi = require('dota2-gsi');
+// var server = new d2gsi(
+//   {
+//     port: process.env.PORT || 3000
+//   }
+// );
 
-server.events.on('newclient', function(client) {
-    console.log("New client connection, IP address: " + client.ip);
-    if (client.auth && client.auth.token) {
-        console.log("Auth token: " + client.auth.token);
-    } else {
-        console.log("No Auth token");
-    }
+// server.events.on('newclient', function(client) {
+//     console.log("New client connection, IP address: " + client.ip);
+//     if (client.auth && client.auth.token) {
+//         console.log("Auth token: " + client.auth.token);
+//     } else {
+//         console.log("No Auth token");
+//     }
 
-    client.on('player:activity', function(activity) {
-        if (activity == 'playing') console.log("Game started!");
-    });
-    client.on('hero:level', function(level) {
-        console.log("Now level " + level);
-        return sendEventsToAll(`Now level ${level}`);
-    });
-    client.on('abilities:ability0:can_cast', function(can_cast) {
-        if (can_cast) console.log("Ability0 off cooldown!");
-    });
-});
+//     client.on('player:activity', function(activity) {
+//         if (activity == 'playing') console.log("Game started!");
+//     });
+//     client.on('hero:level', function(level) {
+//         console.log("Now level " + level);
+//         return sendEventsToAll(`Now level ${level}`);
+//     });
+//     client.on('abilities:ability0:can_cast', function(can_cast) {
+//         if (can_cast) console.log("Ability0 off cooldown!");
+//     });
+// });
