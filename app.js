feb120af7e20d596add679cf2cffa7c0ba33f566
Andrew <andrewwee97@gmail.com>
2022-01-23 03:29:41 -0500

    ..

--- app.js
+++ app.js
@@ -248,7 +248,7 @@ events.on('newclient', async function(client) {
                     {
                         headers: {
                             'Authorization': `Bearer ${jwtToken}`,
-                            'client-id': `${Buffer.from(process.env.TWITCH_CLIENT_ID, 'base64')}`,
+                            'Client_Id': `${Buffer.from(process.env.TWITCH_CLIENT_ID, 'base64')}`,
                             'Content_Type': 'application/json',
                         }
                     }
